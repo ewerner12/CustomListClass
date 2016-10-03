476a34fb0d6ca115a6047cd1e8bd8c07bb53aeb4
Eric Werner <eric.werner09@gmail.com>
2016-10-03 07:45:08 -0500

    first commit

---
+++ ListAssignment/MyList.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//// The built-in List<T> class is a generic class that acts as a wrapper over the array class. [DONE]
+//// Task: Build a custom generic list class that stores its values in an array. [DONE]
+//// Task: Write a method to add an object. [DONE]
+//// Task: Write a method to remove an object. [DONE]
+//// Task: Make sure your class is iterable. [DONE]
+//// Task: Override the ToString Method that converts the contents of the custom list to a string. [DONE]
+//// Task: Overload the + Operator in order to allow us to add two instances of your custom list class. [DONE]
+//// Task: Overload the – Operator in order to allow us to subtract one custom list from another. [DONE]
+//// Task: Implement a Count property. [DONE]
+//// Task: Create a method to zipper two custom lists. [DONE]
+//// 10 Bonus Points: 
+////    Sorting.You must tell us what sorting algorithm you used.You cannot use Array.Sort() for this. []
+
+namespace ListAssignment
+{
+    public class MyList<T> : IEnumerable
+    {
+        private static int arraySize = 10;
+        private T[] items = new T[arraySize];
+        private int itemCounter = 0;
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int index = 0; index < itemCounter; index++)
+            {
+                yield return items[index];
+            }
+        }
+
+        public int Count()
+        {
+            return itemCounter;
+        }
+
+        public void Add(T item)
+        {
+            if(itemCounter >= arraySize)
+            {
+                T[] tempItems = new T[arraySize + 10];
+                for (int i = 0; i < itemCounter; i++)
+                {
+                    tempItems[i] = items[i];
+                }
+                items = new T[arraySize + 10];
+                items = tempItems;
+            }
+            items[itemCounter++] = item;
+        }
+
+        public void RemoveAt(int counterInput)
+        {
+            for (int i = counterInput; i < itemCounter; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            // resize array to fit new list of items (less item removed) //
+            ////T[] newItems = new T[itemCounter - 1];
+            ////newItems = items;
+            itemCounter--;
+        }
+
+        public void Remove(T itemMatch)
+        {
+            T[] newItems = new T[itemCounter];
+            int matchesFound = 0;
+
+            for(int i = 0; i < itemCounter; i++)
+            {
+                if (items[i].Equals(itemMatch))
+                {
+                    matchesFound++;
+                }
+                else
+                {
+                    newItems[i - matchesFound] = items[i];
+                }
+            }
+            items = newItems;
+            itemCounter -= matchesFound;
+        }
+
+        public override string ToString()
+        {
+            string itemList = null;
+
+            for (int i = 0; i < itemCounter; i++)
+            {
+                itemList += items[i] + " : ";
+            }
+            return itemList;
+        }
+
+        public static MyList<T> operator +(MyList<T> firstList, MyList<T> secondList)
+        {
+            MyList<T> combinedList = new MyList<T>();
+
+            for (int i = 0; i < firstList.Count(); i++)
+            {
+                combinedList.Add(firstList.items[i]);
+            }
+            for (int j = 0; j < secondList.Count(); j++)
+            {
+                combinedList.Add(secondList.items[j]);
+            }
+
+            return combinedList;
+        }
+
+        public static MyList<T> operator -(MyList<T> firstList, MyList<T> secondList)
+        {
+            for (int i = 0; i < secondList.Count(); i++)
+            {
+                firstList.Remove(secondList.items[i]);
+            }
+            return firstList;
+            
+            ////following will subtract shorter list from longer list and return resulting list
+            //MyList<T> subtractedList = new MyList<T>();
+
+            //if (firstList.Count() > secondList.Count())
+            //{
+            //    for (int i = 0; i < firstList.Count(); i++)
+            //    {
+            //        firstList.Remove(secondList.items[i]);
+            //    }
+            //    return firstList;
+            //}
+            //else if (firstList.itemCounter < secondList.itemCounter)
+            //{
+            //    for (int i = 0; i < secondList.Count(); i++)
+            //    {
+            //        secondList.Remove(firstList.items[i]);
+            //    }
+            //    return secondList;
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < firstList.Count(); i++)
+            //    {
+            //        firstList.Remove(secondList.items[i]);
+            //    }
+            //    return firstList;
+            //}
+        }
+
+        public MyList<T> ZipperTwoLists(MyList<T> firstList, MyList<T> secondList)
+        {
+            MyList<T> zipperedList = new MyList<T>();
+
+            if (firstList.Count() > secondList.Count())
+            {
+                for (int i = 0; i < firstList.Count(); i++)
+                {
+                    zipperedList.items[i] = GetZippedItem(firstList.items[i], secondList.items[i]);
+                }
+            }
+            else if (firstList.itemCounter < secondList.itemCounter)
+            {
+                for (int i = 0; i < secondList.Count(); i++)
+                {
+                    zipperedList.items[i] = GetZippedItem(firstList.items[i], secondList.items[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < firstList.Count(); i++)
+                {
+                    zipperedList.items[i] = GetZippedItem(firstList.items[i], secondList.items[i]);
+                }
+            }
+
+            return zipperedList;
+        }
+
+        public T GetZippedItem(T firstItem, T secondItem)
+        {
+            //should check for nulls
+
+            string zippedItem;
+            zippedItem = firstItem + " : " + secondItem;
+
+            return (T)(object)zippedItem;
+        }
+
+        ////public MyList<T> GetSortedList(MyList<T> unsortedList)
+        ////{
+        ////    T tempItem = default(T);
+
+        ////    for (int i = 0; i < unsortedList.Count(); i++)
+        ////    {
+
+        ////    }
+        ////}
+
+    }
+}
---
+++ ListAssignment/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//// The built-in List<T> class is a generic class that acts as a wrapper over the array class
+//// Task: Build a custom generic list class that stores its values in an array.
+//// Task: Write a method to add an object.
+//// Task: Write a method to remove an object
+//// Task: Make sure your class is iterable
+//// Task: Override the ToString Method that converts the contents of the custom list to a string.
+//// Task: Overload the + Operator in order to allow us to add two instances of your custom list class
+//// Task: Overload the – Operator in order to allow us to subtract one custom list from another
+//// Task: Implement a Count property
+//// Task: Create a method to zipper two custom lists.
+//// 10 Bonus Points: Sorting.You must tell us what sorting algorithm you used.You cannot use Array.Sort() for this.
+
+namespace ListAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //////List<string> newList = new List<string>();
+            //////newList.Add("abc");
+            //////newList.Add("DEF");
+            //////newList.Add("ghi");
+            //////newList.Add("TEST");
+
+            MyList<int> sampleList = new MyList<int>();
+            sampleList.Add(1);
+            sampleList.Add(2);
+            sampleList.Add(3);
+            sampleList.Add(4);
+            sampleList.Add(5);
+            sampleList.Add(6);
+            sampleList.Add(7);
+            sampleList.Add(8);
+
+            MyList<int> sample2List = new MyList<int>();
+            sample2List.Add(9);
+            sample2List.Add(10);
+            sample2List.Add(2);
+            sample2List.Add(4);
+            sample2List.Add(6);
+            sample2List.Add(8);
+
+            MyList<int> combinedList = new MyList<int>();
+            combinedList = (sampleList + sample2List);
+            foreach (int i in combinedList)
+            {
+                Console.Write("{0} : ", i);
+            }
+
+            MyList<string> stringList = new MyList<string>();
+            stringList.Add("abc");
+            stringList.Add("DEF");
+            stringList.Add("ghi");
+            stringList.Add("JKL");
+            stringList.Add("mno");
+            stringList.Add("abc");
+            stringList.Add("PQR");
+            stringList.Add("stu");
+
+            //foreach (int i in sampleList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Array length = {0} ", sampleList.Count());
+            Console.WriteLine("");
+            Console.WriteLine("");
+            foreach (string i in stringList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Array length = {0} ", stringList.Count());
+            Console.WriteLine("");
+
+            stringList.Remove("abc");
+
+            foreach (string i in stringList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Array length = {0} ", stringList.Count());
+            Console.WriteLine("");
+
+            Console.WriteLine(sampleList);
+            Console.WriteLine(stringList);
+            Console.WriteLine("");
+
+            MyList<int> subtractedList = new MyList<int>();
+            subtractedList = (sampleList - sample2List);
+            Console.WriteLine("Subtracted list: ");
+            foreach (int i in subtractedList)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            ////////removing item by index////////
+            //sampleList.RemoveAt(1);
+            //foreach (int i in sampleList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Array length = {0} ", sampleList.Count());
+
+            //sampleList.RemoveAt(2);
+            //foreach (int i in sampleList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Array length = {0} ", sampleList.Count());
+
+            //sampleList.RemoveAt(3);
+            //foreach (int i in sampleList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Array length = {0} ", sampleList.Count());
+
+            //sampleList.RemoveAt(4);
+            //foreach (int i in sampleList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Array length = {0} ", sampleList.Count());
+            ////////removing item by index////////
+
+            ////////removing item by value////////
+            //sampleList.Remove(5);
+            //foreach (int i in sampleList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Array length = {0} ", sampleList.Count());
+            ////////removing item by value////////
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
